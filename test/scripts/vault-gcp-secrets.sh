ee12af803bd52c981c473240ef3e44c6be1a6678
Tommy McNeely <thomas.mcneely@davita.com>
2022-01-11 18:10:37 -0700

    fix: add retry loop on GCP Secrets Roleset

--- test/scripts/vault-gcp-secrets.sh
+++ test/scripts/vault-gcp-secrets.sh
@@ -15,7 +15,9 @@ vault write gcp/config \
 PROJECT_ID=$(echo "${GCP_CREDENTIALS}" | jq -r '.project_id')
 echo "::set-output name=project_id::${PROJECT_ID}"
 
-vault write gcp/roleset/vault-gcp-secrets \
+try=1
+maxtries=3
+until vault write gcp/roleset/vault-gcp-secrets \
   project="${PROJECT_ID}" \
   secret_type="service_account_key" \
   bindings=-<<EOF
@@ -23,6 +25,15 @@ vault write gcp/roleset/vault-gcp-secrets \
     roles = ["roles/viewer"]
   }
 EOF
+do
+  if [ $try -ge $maxtries ]; then
+    >&2 echo "ERROR - Unable to create vault GCP Roleset after ${try} tries."
+    exit 1
+  fi
+  echo -n "ERROR Creating roleset... sleeping ${try} seconds"
+  sleep $((try++))
+done
+
 
 SERVICE_ACCOUNT_EMAIL=$(vault read -field service_account_email gcp/roleset/vault-gcp-secrets)
 echo "::set-output name=service_account_email::${SERVICE_ACCOUNT_EMAIL}"
